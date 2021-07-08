17a1be8091a6634b0d2c7921adc4b6b409116272
Emilie Bojkovic <emilie.bojkovic@outlook.com>
2021-07-08 16:23:26 +0200

    Updating the code

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-/*Suite de Fibonacci : chaque terme est la somme des 2 termes qui précèdent*/
-let suite = document.getElementById('suite');
+//Suite de Fibonacci
 
-function fibonacci(number){ 
-    let number1 = 0; //je déclare mon 1er chiffre
-    let number2 = 1; //je déclare mon 2ème chiffre
-    let sum; // je déclare la somme
+let fibonacci = (number) => {
+    let number1 = 0; //je déclare mon premier chiffre
+    let number2 = 1; //je déclare mon deuxième chiffre
+    let sum; //je déclare le total 
 
-    //utilisation d'une boucle
-    for (let index = 0; index < number; index++) {
+    //j'utilise for pour parcourir la boucle
+    for (let index = 2; index <= number; index++) {
         sum = number1 + number2;
         number1 = number2;
-        number2 = sum;
+        number2 = sum;        
     }
     return number2;
 }
+console.log(fibonacci(8));
 
-suite.textContent= 'POur le chiffre 5, la fonction retourne : ' + fibonacci(5);
 
+//Récupérer les données d'un API
 
-/*Fonction d'appel à l'API GitHub*/
-let api = document.getElementById('api');
-
-function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest(); // envoie d'un erequête HTTP
-    xmlHttp.open( "GET", url, false ); // methode de requête + url + processus effectué de manière synchrone avec false
-    xmlHttp.send( null );
+let httpGet = (url) => {
+    let xmlHttp = new XMLHttpRequest(); //envoi d'une requête HTTP
+    xmlHttp.open('GET', url, false); //open() instancie une nouvelle requête 
+    xmlHttp.send(null) ; //envoie la requête au serveur
     return xmlHttp.responseText;
 }
-
-api.innerHTML = 'Voici ce que retourne l\'Api de GitHub pour l\'utilisateur AlavaB :' + '</br>' + '</br>' + (httpGet("https://api.github.com/users/alavab")); //appel de mon utilisateur GitHub
+console.log(httpGet("https://api.github.com/users/alavab"));
